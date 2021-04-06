d9afd306b2f62259605f299cc81a7bd7b45c7cb5
Ubuntu <ubuntu@ip-172-31-5-238.ap-northeast-2.compute.internal>
2021-04-06 11:15:14 +0000

    update to study Sphinx

--- adam.py
+++ adam.py
@@ -1,1 +1,30 @@
-print('hello')
+"""Just for practicing Sphinx"""
+
+class adam:
+    """연습용 클래스"""
+    def __init__(self):
+        """생성자 함수"""
+        print("생성")
+
+    def sum_two(self, a, b):
+        """Compute sum of two values, and return int"""
+        return a + b
+
+    def get_biggest(self, x):
+        """Return the biggest value
+
+        :param x: list of int
+        :type x: list
+        :return: The biggest int value
+        :rtype: int
+        """
+
+        ret = x[0]
+        for i in range(1, len(x)):
+            if x[i] > ret:
+                ret = x[i]
+        return ret
+
+a = adam()
+print("sum_two() return = ", a.sum_two(1, 3))
+print("get_biggest() return = ", a.get_biggest([5, 3, 4, -10, 0]))
